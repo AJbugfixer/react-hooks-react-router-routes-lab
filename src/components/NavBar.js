c2794201a045ce22d30736f7996723b1b4bee467
Ajay <ajay.ip3105@gmail.com>
2022-06-17 01:01:13 -0400

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,35 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  const styleMenu = {
+  display: "inline-block",
+  font: "Arial",
+  margin:"0 5px 5px",
+  padding: "10px",
+  border: "solid white",    
+  background: "IndianRed",
+  textDecoration:"underline",
+  color : "silver",
+  fontsize: "20px",
+
+  }
+
+  return(
+    <div className="navbar">
+      <NavLink to="/" exact style={styleMenu}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" style={styleMenu}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={styleMenu}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" style={styleMenu}>
+        Actors
+      </NavLink>
+    </div>
+  )
 }
 
 export default NavBar;
